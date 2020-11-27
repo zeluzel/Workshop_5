f44fbef1dfd9057cb5a11ca11ca11cb5fe83d894
zeluzel <zeluzel@gmail.com>
2020-11-27 14:44:29 +0100

    basic configuration

---
+++ src/main/java/pl/coderslab/workshophibernate/WorkshopHibernateApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.workshophibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/workshophibernate/WorkshopHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.workshophibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
